94a92e9aad7968b8ac56fb611153030d63fc8e9a
timbriant <tbsc.09@gmail.com>
2019-12-10 16:24:03 +0800

    added remittance chart

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import KML from 'ol/format/KML';
-import {Heatmap as HeatmapLayer, Tile as TileLayer} from 'ol/layer';
+import { Heatmap as HeatmapLayer, Tile as TileLayer } from 'ol/layer';
 import Stamen from 'ol/source/Stamen';
 import VectorSource from 'ol/source/Vector';
+import Chart from 'chart.js';
 
 var vector = new HeatmapLayer({
   source: new VectorSource({
@@ -15,7 +16,7 @@ var vector = new HeatmapLayer({
   }),
   blur: parseInt(40, 10),
   radius: parseInt(15, 10),
-  weight: function(feature) {
+  weight: function (feature) {
     var name = feature.get('name');
     var magnitude = parseFloat(name.substr(2));
     return magnitude - 5;
@@ -39,6 +40,41 @@ new Map({
   })
 });
 
-map.on('moveend', function(e) {
-  console.log("hello");
+
+var ctx = document.getElementById('myChart').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+  responsive: true,
+  scaleFontColor: 'white',
+
+  // The data for our dataset
+  data: {
+
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [{
+      fill: true,
+      label: 'My First dataset',
+      pointBorderColor: "white",
+      borderColor: 'rgb(255, 255, 255)',
+      data: [50, 40, 13, 7, 9, 29, 31, 28, 33, 35, 40, 70],
+      pointBackgroundColor: 'white',
+
+
+    }]
+  },
+  options: {
+    elements: {
+      line: {
+        tension: 0
+      }
+    },
+    title: {
+      display: false,
+    },
+    legend: {
+      display: false,
+    },
+  }
 });
